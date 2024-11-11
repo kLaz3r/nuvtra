d500089a41454317307b15bcd05df000e3097bd3
stefan.nasturas <klazer220033@gmail.com>
2024-11-11 07:38:34 +0200

    added navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,13 +2,26 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
+  darkMode: ["class"],
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        text: "rgba(245, 241, 239, 1)",
+        accent: "rgba(120, 140, 173, 1)",
+        secondary: "rgba(66, 109, 98, 1)",
+        background: "rgba(10, 7, 6, 1)",
+        primary: "rgba(235, 94, 40, 1)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
